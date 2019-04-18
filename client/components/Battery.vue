b1c9f9cb103c38bf12c20ccfb12cbca74ca307e5
Jordan Mavrogeorge <jordomav@gmail.com>
2019-04-17 21:37:02 -0700

    create battery component

---
+++ client/components/Battery.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="battery">
+        <div :style="'flex-basis:' + level + '%; background-color:' + getColor + ';'"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'nuxt-property-decorator';
+
+@Component
+export default class Battery extends Vue {
+    @Prop({ required: true }) level!: string;
+
+    get getColor() {
+        if (this.level > 50) { return 'green' }
+        else if ((this.level < 50) && (this.level > 25)) { return 'yellow' }
+        else if (this.level < 25) { return 'red' }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .battery {
+        display: flex;
+        height: 50px;
+        width: 200px;
+        border: 3px solid #000;
+    }
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,16 +1,29 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="container">
+        <battery level="49"/>
     </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'nuxt-property-decorator';
+import Battery from '~/components/Battery.vue';
 
-@Component
+@Component({
+    components: { Battery }
+})
 export default class Home extends Vue {
+    $socket: any;
+
+    mounted() {
+        this.$socket.on('state', (message) => {
+            console.log(message);
+        })
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+    .container {
+        display: grid;
+    }
 </style>
